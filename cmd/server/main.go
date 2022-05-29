2f65f69adbfb987f1b06ef92325b11c237f4a215
esma-yigit <75517864+esma-yigit@users.noreply.github.com>
2022-05-29 22:14:57 +0300

    structuring app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+//App => the struct which contains things like pointers
+//to database connection
+type App struct {
+}
+
+func (a *App) Run() error {
+	fmt.Println("Setting up our App")
+	return nil
+}
+
+//Run => sets up our application
+
 func main() {
 	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our application")
+		fmt.Println(err)
+	}
 }
